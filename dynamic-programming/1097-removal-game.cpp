b20c262dbd73c9a3c5d2b8667d7f70c466c21dba
gogalvijay <gogalvijay@gmail.com>
2026-01-08 16:48:52 +0530

    added 1097

---
+++ dynamic-programming/1097-removal-game.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define n_l '\n'
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; cout << to_string(__VA_ARGS__) << endl
+template <typename T, size_t N> 
+int SIZE(const T (&t)[N])
+{ 
+    return N; 
+} 
+template<typename T> 
+int SIZE(const T &t)
+{ 
+    return t.size(); 
+} 
+string to_string(const string s, int x1=0, int x2=1e9)
+{ 
+    return '"' + ((x1 < s.size()) ? s.substr(x1, x2-x1+1) : "") + '"'; 
+} 
+string to_string(const char* s) 
+{ 
+    return to_string((string) s); 
+} 
+string to_string(const bool b) 
+{ 
+    return (b ? "true" : "false"); 
+} 
+string to_string(const char c)
+{ 
+    return string({c}); 
+} 
+template<size_t N> 
+string to_string(const bitset<N> &b, int x1=0, int x2=1e9)
+{ 
+    string t = ""; 
+    for(int __iii__ = min(x1,SIZE(b)),  __jjj__ = min(x2, SIZE(b)-1); __iii__ <= __jjj__; ++__iii__)
+    { 
+        t += b[__iii__] + '0'; 
+    } 
+    return '"' + t + '"'; 
+} 
+template <typename A, typename... C> 
+string to_string(const A (&v), int x1=0, int x2=1e9, C... coords); 
+int l_v_l_v_l = 0, t_a_b_s = 0; 
+template <typename A, typename B> 
+string to_string(const pair<A, B> &p) 
+{ 
+    l_v_l_v_l++; 
+    string res = "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; l_v_l_v_l--; 
+    return res; 
+} 
+template <typename A, typename... C> 
+string to_string(const A (&v), int x1, int x2, C... coords) 
+{ 
+    int rnk = rank<A>::value; string tab(t_a_b_s, ' '); 
+    string res = ""; 
+    bool first = true; 
+    if(l_v_l_v_l == 0) 
+        res += n_l; 
+   res += tab + "["; 
+   x1 = min(x1, SIZE(v)), x2 = min(x2, SIZE(v)); 
+   auto l = begin(v); 
+   advance(l, x1); 
+   auto r = l; 
+   advance(r, (x2-x1) + (x2 < SIZE(v))); 
+   for (auto e = l; e != r; e = next(e)) 
+   { 
+        if (!first)
+        { 
+            res += ", "; 
+        } 
+        first = false; 
+        l_v_l_v_l++; 
+        if(e != l)
+        { 
+            if(rnk > 1) 
+            {
+                 res += n_l; 
+                 t_a_b_s = l_v_l_v_l; 
+            }; 
+       } 
+       else{
+        t_a_b_s = 0; 
+      } 
+      res += to_string(*e, coords...); 
+      l_v_l_v_l--; 
+    } 
+    res += "]"; 
+    if(l_v_l_v_l == 0) 
+        res += n_l; 
+    return res; 
+} 
+void dbgm(){;} 
+template<typename Heads, typename... Tails>
+void dbgm(Heads H, Tails... T)
+{ 
+    cout << to_string(H) << " | "; dbgm(T...); 
+} 
+#define dbgm(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgm(__VA_ARGS__); cout << endl
+void prepare(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // #ifndef ONLINE_JUDGE
+        // freopen("input.txt", "r", stdin);
+        // freopen("output.txt", "w", stdout);
+    // #endif 
+}
+vector<long long>pre;
+vector<vector<long long>>dp;
+void solve() {
+    int n;
+    cin>>n;
+    vector<long long>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    pre=a;
+    for(int i=1;i<n;i++)
+        pre[i]+=pre[i-1];
+    //dbg(pre);
+    dp.resize(n,vector<long long>(n));
+    for(int l=n-1;l>=0;l--){
+        for(int r=l;r<n;r++){
+            if(l==r){
+                dp[l][l]=a[l];
+                continue;
+            }
+            else{
+                //l to r max(dp[l+1][r],dp[l][r-1])+a[l];
+                long long left=pre[r]-pre[l];
+                long long right;
+                if(l==0)
+                    right=pre[r-1];
+                else
+                    right=pre[r-1]-pre[l-1];
+                dp[l][r]=max(a[l]+(left-dp[l+1][r]),a[r]+(right-dp[l][r-1]));
+            }
+        }
+    }
+    //dbg(dp);
+    cout<<dp[0][n-1];
+}
+int main()
+{
+    prepare();
+    ll t=1;
+    //cin>>t;
+    while(t--){
+       solve();
+    }
+    return 0;
+}
+ 
