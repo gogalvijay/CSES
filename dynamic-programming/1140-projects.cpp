7af798789e4d68e4ffc8a911a3e1735321dc6477
gogalvijay <gogalvijay@gmail.com>
2026-01-08 16:49:21 +0530

    added 1140

---
+++ dynamic-programming/1140-projects.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define n_l '\n'
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; cout << to_string(__VA_ARGS__) << endl
+template <typename T, size_t N> 
+int SIZE(const T (&t)[N])
+{ 
+    return N; 
+} 
+template<typename T> 
+int SIZE(const T &t)
+{ 
+    return t.size(); 
+} 
+string to_string(const string s, int x1=0, int x2=1e9)
+{ 
+    return '"' + ((x1 < s.size()) ? s.substr(x1, x2-x1+1) : "") + '"'; 
+} 
+string to_string(const char* s) 
+{ 
+    return to_string((string) s); 
+} 
+string to_string(const bool b) 
+{ 
+    return (b ? "true" : "false"); 
+} 
+string to_string(const char c)
+{ 
+    return string({c}); 
+} 
+template<size_t N> 
+string to_string(const bitset<N> &b, int x1=0, int x2=1e9)
+{ 
+    string t = ""; 
+    for(int __iii__ = min(x1,SIZE(b)),  __jjj__ = min(x2, SIZE(b)-1); __iii__ <= __jjj__; ++__iii__)
+    { 
+        t += b[__iii__] + '0'; 
+    } 
+    return '"' + t + '"'; 
+} 
+template <typename A, typename... C> 
+string to_string(const A (&v), int x1=0, int x2=1e9, C... coords); 
+int l_v_l_v_l = 0, t_a_b_s = 0; 
+template <typename A, typename B> 
+string to_string(const pair<A, B> &p) 
+{ 
+    l_v_l_v_l++; 
+    string res = "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; l_v_l_v_l--; 
+    return res; 
+} 
+template <typename A, typename... C> 
+string to_string(const A (&v), int x1, int x2, C... coords) 
+{ 
+    int rnk = rank<A>::value; string tab(t_a_b_s, ' '); 
+    string res = ""; 
+    bool first = true; 
+    if(l_v_l_v_l == 0) 
+        res += n_l; 
+   res += tab + "["; 
+   x1 = min(x1, SIZE(v)), x2 = min(x2, SIZE(v)); 
+   auto l = begin(v); 
+   advance(l, x1); 
+   auto r = l; 
+   advance(r, (x2-x1) + (x2 < SIZE(v))); 
+   for (auto e = l; e != r; e = next(e)) 
+   { 
+        if (!first)
+        { 
+            res += ", "; 
+        } 
+        first = false; 
+        l_v_l_v_l++; 
+        if(e != l)
+        { 
+            if(rnk > 1) 
+            {
+                 res += n_l; 
+                 t_a_b_s = l_v_l_v_l; 
+            }; 
+       } 
+       else{
+        t_a_b_s = 0; 
+      } 
+      res += to_string(*e, coords...); 
+      l_v_l_v_l--; 
+    } 
+    res += "]"; 
+    if(l_v_l_v_l == 0) 
+        res += n_l; 
+    return res; 
+} 
+void dbgm(){;} 
+template<typename Heads, typename... Tails> 
+void dbgm(Heads H, Tails... T)
+{ 
+    cout << to_string(H) << " | "; dbgm(T...); 
+} 
+#define dbgm(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgm(__VA_ARGS__); cout << endl
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<vector<long long>>vec(n,vector<long long>(3));
+    for (int i = 0;i<n;i++){
+        cin >> vec[i][1] >> vec[i][0] >> vec[i][2];
+    }
+    sort(vec.begin(),vec.end());
+    vector<long long> dp(n);
+    long long ans ;
+    dp[0] = vec[0][2];
+    ans = dp[0];
+    for (int i = 1; i < n;i++){
+        //cout << i << ' ';
+        int l = 0, h = i - 1,mid,j=-1;
+        while(l<=h){
+            mid = (l + h) / 2;
+            if(vec[i][1]>vec[mid][0]){
+                l = mid + 1;
+                j = mid;
+            }
+                else h = mid - 1;
+        }
+        long long curr;
+        if(j==-1)
+            curr = vec[i][2];
+        else
+            curr=dp[j] + vec[i][2];
+
+        dp[i] = max(dp[i - 1], curr);
+        ans = max(ans, dp[i]);
+    }
+    //dbg(dp);
+    cout << ans;
+}
+int main()
+{
+    int t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+}
+ 
+
